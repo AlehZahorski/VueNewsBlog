c7df0686c025f13a011191b478df08540bd42e88
alehZahorski <aleh.zahorski@machineportal.com>
2021-04-20 20:16:32 +0200

    realisation pagination with query-string, some fix code

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,9 +1,60 @@
 <template>
-  <div>Feed {{ apiUrl }}</div>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happed</div>
+
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username},
+              }"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">ADD TO FAVORITES</div>
+        </div>
+        <router-link
+          :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      <mcv-pagination
+        :total="feed.articlesCount"
+        :limit="limit"
+        :current-page="currentPage"
+        :url="baseUrl"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapState} from 'vuex';
+
 import {actionTypes} from '@/store/modules/feed';
+import McvPagination from '@/components/Pagination';
+import {limit} from '@/helpers/vars';
+import {stringify, parseUrl} from 'query-string';
 
 export default {
   name: 'McvFeed',
@@ -13,9 +64,51 @@ export default {
       required: true,
     },
   },
+  components: {
+    McvPagination,
+  },
+  data() {
+    return {
+      limit,
+      url: '/',
+    };
+  },
+  computed: {
+    ...mapState({
+      isLoading: (state) => state.feed.isLoading,
+      feed: (state) => state.feed.data,
+      error: (state) => state.feed.error,
+    }),
+    currentPage() {
+      return Number(this.$route.query.page || '1');
+    },
+    baseUrl() {
+      return this.$route.path;
+    },
+    offset() {
+      return this.currentPage * limit - limit;
+    },
+  },
+  watch: {
+    currentPage() {
+      this.fetchFeed();
+    },
+  },
   mounted() {
-    console.log('feed');
-    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl});
+    this.fetchFeed();
+  },
+  methods: {
+    fetchFeed() {
+      const parsedUrl = parseUrl(this.apiUrl);
+      const stringifiedParams = stringify({
+        limit,
+        offset: this.offset,
+        ...parsedUrl.query,
+      });
+      const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`;
+      console.log(apiUrlWithParams);
+      this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams});
+    },
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,11 @@ import VueRouter from 'vue-router';
 import GlobalFeed from '../views/GlobalFeed';
 import Register from '@/views/Register';
 import Login from '@/views/Login';
+import Feed from '@/components/Feed';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'globalFeed',
-    component: GlobalFeed,
-  },
   {
     path: '/register',
     name: 'register',
@@ -21,6 +17,51 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login,
+  },
+  {
+    path: '/',
+    name: 'globalFeed',
+    component: GlobalFeed,
+  },
+  {
+    path: '/feed',
+    name: 'feed',
+    component: Feed,
+  },
+  {
+    path: '/tags/:slug',
+    name: 'tag',
+    component: Feed,
+  },
+  {
+    path: '/articles/new',
+    name: 'createArticle',
+    component: Feed,
+  },
+  {
+    path: '/article/:slug',
+    name: 'article',
+    component: Feed,
+  },
+  {
+    path: '/article/:slug/edit',
+    name: 'editArticle',
+    component: Feed,
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: Feed,
+  },
+  {
+    path: '/profiles/:slug',
+    name: 'userProfile',
+    component: Feed,
+  },
+  {
+    path: '/profiles/:slug/favorites',
+    name: 'userProfileFavorites',
+    component: Feed,
   },
 ];
 
